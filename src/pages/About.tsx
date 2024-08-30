a39893b908f66b1216bf31803399d49f6c5e11db
kjlee2497 <kjlee2497@gmail.com>
2024-08-30 19:07:30 -0400

    adding carousel wip

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,51 @@
 import Navbar from '../Components/navbar'
 import Footer from '../Components/footer'
 
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+
 const About = () => {
+
+
   return (
-    <div>
+
+
+    <div className="main">
       <Navbar />
-      
+
+      {/* 
+  For swiper, use a map method to create a slide for each part of an array of objects (headshots and descriptions)
+  look into documentation for ISACTIVE boolean
+  use this boolean to get the aesthetics you want
+*/}
+      <div className='swiper-container'>
+        <h2>Meet Our Staff</h2>
+        <Swiper
+          // install Swiper modules
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={0}
+          slidesPerView={3}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log('slide change')}
+        >
+          <SwiperSlide><img src="christian headshot.jpg" alt="" /></SwiperSlide>
+          <SwiperSlide><img src="christian headshot.jpg" alt="" /></SwiperSlide>
+          <SwiperSlide><img src="christian headshot.jpg" alt="" /></SwiperSlide>
+          <SwiperSlide><img src="christian headshot.jpg" alt="" /></SwiperSlide>
+        </Swiper>
+      </div>
+
       <Footer />
     </div>
   )
